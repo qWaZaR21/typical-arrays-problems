7eb2f2e1c6d7213b13def399eaa12612ef83b62e
Pavel Kulesh <kulesh.p.v.b.2.m@gmail.com>
2020-03-02 18:22:14 +0300

    add typical-arrays-problems solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (!array || array.length == 0) return 0; 
+  let minimal = array[0];
+  result = array.reduce((minimal, value) => {
+    if (minimal > value) minimal = value;
+    return minimal;
+  }, minimal);
+  return result;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!array || array.length == 0) return 0; 
+  let maximal = array[0];
+  result = array.reduce((maximal, value) => {
+    if (maximal < value) maximal = value;
+    return maximal;
+  }, maximal);
+  return result;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!array || array.length == 0) return 0; 
+  let sum = 0;
+  result = array.map(value => {
+    sum = sum + value;
+  });
+  return sum/array.length;
 }
